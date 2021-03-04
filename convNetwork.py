82018edbb898132dbb26af6e38f9036cd7dfe8ec
rrbaeta <52976056+rrbaeta@users.noreply.github.com>
2021-03-04 15:24:00 +0000

    opencv2
    opencv2

--- convNetwork.py
+++ convNetwork.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 import numpy as np
 import sys
-import cv2 as cv
+from cv2 import cv2 as cv
 from tensorflow.keras import datasets, layers, models
 
 #-------------------------------------------------INFO--------------------------------------------------
